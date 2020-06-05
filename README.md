a12bec1fb3f00fb0a5431aa7bf645e513dd34c6c
sweta sengupta <ssengupta801@gmail.com>
2020-06-05 18:52:19 -0500

    recreated the lecture files

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
 # twittoff-ssg
+# Setup
+
+```
+pipenv install
+```
+Migrate  the database
+
+```sh
+$env:FLASK_APP = "web_app"
+flask db init
+
+$env:FLASK_APP = "web_app"
+flask db migrate
+
+$env:FLASK_APP = "web_app"
+flask db upgrade
+
+#Usage
+```sh
+$env:FLASK_APP = "web_app"
+flask run
+```
---
+++ web_app/services/basilica_service.py
@@ -0,0 +1,16 @@
+#web_app/services/basilica_services.py
+import basilica
+import os
+#from dotenv import load_dotenv
+
+#load_dotenv() # parse the .env file for the environmental variables
+
+#BASILICA_API_KEY = os.getenv("BASILICA_API_KEY")
+
+connection = basilica.Connection('a0a0c93b-f246-41bd-6f06-101e687ae06c')
+print(type(connection))
+
+if __name__ == "__main__":
+    sentences = ["Hello world!", "How are you?"]
+    embeddings = connection.embed_sentences(sentences)
+    print(list(embeddings)) # [[0.8556405305862427, ...], ...]
