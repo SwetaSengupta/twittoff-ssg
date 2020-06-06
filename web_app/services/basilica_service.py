575de2bad8aa021aa8824ed6d241b630e9e7aeae
sweta sengupta <ssengupta801@gmail.com>
2020-06-05 20:38:49 -0500

    cleaned the file a bit

--- web_app/services/basilica_service.py
+++ web_app/services/basilica_service.py
@@ -1,13 +1,14 @@
 #web_app/services/basilica_services.py
 import basilica
 import os
-#from dotenv import load_dotenv
+from dotenv import load_dotenv
 
-#load_dotenv() # parse the .env file for the environmental variables
+load_dotenv() # parse the .env file for the environmental variables
 
-#BASILICA_API_KEY = os.getenv("BASILICA_API_KEY")
+BASILICA_API_KEY = os.getenv("BASILICA_API_KEY")
 
-connection = basilica.Connection('a0a0c93b-f246-41bd-6f06-101e687ae06c')
+#connection = basilica.Connection('a0a0c93b-f246-41bd-6f06-101e687ae06c')
+connection = basilica.Connection(BASILICA_API_KEY)
 print(type(connection))
 
 if __name__ == "__main__":
